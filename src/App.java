b9e7a30d7e1d4e50270f189a85bff883c1d480f3
Shubham Kumar Sinha <shubhamsinha2009@gmail.com>
2022-09-27 12:53:08 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Print Stars Pattern 
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        
+    }
+}

6926f0c5a4a39ae3bed0bbf3b64f5a29ecf7ce3c
Shubham Kumar Sinha <shubhamsinha2009@gmail.com>
2022-09-27 13:15:44 +0530

    secondVideo

--- src/App.java
+++ src/App.java
@@ -1,11 +1,28 @@
+
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        // Print Stars Pattern 
+      // Variables
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        
+int a = 10 ;
+int b = 25;
+int sum = a +b;
+System.out.println(sum);
+int difference=  a-b;
+System.out.println(difference);
+
+int mul = a*b;
+System.out.println(mul);
+
+
+   // Input
+
+   Scanner scanner = new Scanner(System.in);
+
+   String name = scanner.nextLine();
+
+   System.out.println(name);
+
     }
 }
